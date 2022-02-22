4b23ad126fd09aea84ad8754c6eae1b3a9a3b83e
Mohammed Rehaan <mrehaan320@gmail.com>
2022-02-22 05:04:03 -0500

    First commit

---
+++ src/main/java/com/example/application/views/list/MainLayout.java
@@ -0,0 +1,54 @@
+package com.example.application.views.list;
+
+import com.example.application.security.SecurityService;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.HighlightConditions;
+import com.vaadin.flow.router.RouterLink;
+
+public class MainLayout extends AppLayout{
+	
+	SecurityService securityService;
+	
+	public MainLayout(SecurityService securityService)
+	{
+		this.securityService = securityService;
+		createHeader();
+		createDrawer();
+	}
+
+	private void createDrawer() {
+		
+		RouterLink listView = new RouterLink("List", ListView.class);
+		listView.setHighlightCondition(HighlightConditions.sameLocation());
+		
+		addToDrawer(new VerticalLayout(
+					listView,
+					new RouterLink("Dashboard",DashboardView.class)
+				));
+		
+	}
+
+	private void createHeader() {
+		H1 logo = new H1("Rehaan CRM");
+		logo.addClassNames("text-l","m-m");
+		
+		Button logOut = new Button("Log out", e ->securityService.logout());
+		
+		HorizontalLayout header = new HorizontalLayout(new DrawerToggle(),logo,logOut);
+		
+		header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+		header.expand(logo);
+		header.setWidthFull();
+		header.addClassNames("py-0","px-m");
+		
+		addToNavbar(header);
+		
+	}
+
+}
